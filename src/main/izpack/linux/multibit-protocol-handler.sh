17f7b3a18c0b3058b0151aa5b4cf187d19581876
MeshCollider <dobsonsa68@gmail.com>
2015-04-24 16:44:07 +1200

    First commit of OmniLite page

--- src/main/izpack/linux/multibit-protocol-handler.sh
+++
@@ -1,3 +0,0 @@
-gconftool-2 -t string -s /desktop/gnome/url-handlers/omnicoin/command "java -splash:doesnotexist.png -jar $INSTALL_PATH/omnilite-exe.jar %s"
-gconftool-2 -s /desktop/gnome/url-handlers/omnicoin/needs_terminal false -t bool
-gconftool-2 -t bool -s /desktop/gnome/url-handlers/omnicoin/enabled true
